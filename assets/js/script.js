e19396ba99589b2684cd658df030353e1814dc00
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-10-29 03:42:47 -0500

    url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("Hello, World!");
+const app = {
+    init: () => {
+        document.getElementById('searchButton');
+        document.addEventListener('click', app.fetchWeather);
+
+},
+fetchWeather: () => {
+    let city = document.getElementById('addCity').value;
+    let key = 'b0c8bb28d93f9d08fb9250393ad62966';
+    let lang = 'en';
+    let units = 'metric';
+    let url = `http://api.openweathermap.org/data/2.5/onecall?city=${city}&appid=${key}&units=${units}&lang=${lang}`;
+    fetch(url)
+    .then((resp) => {
+        if (!resp.ok) throw new Error(resp.statusText);
+        return resp.json();
+      })
+    //   .then((data) => {
+    //     app.showWeather(data);
+    //   })
+      .catch(console.err);
+},
+
+}
+
+app.init();
