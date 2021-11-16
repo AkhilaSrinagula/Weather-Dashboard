cf578e12d142bcea79722dc6d880cdf47b0c634e
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2021-11-16 03:18:14 -0600

    five-day-api

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,21 @@ $(document).ready(function () {
   var temperature = $("#temp");
   var wind = $("#wind");
   var humidity = $("#humidity");
+  var fiveDayTemp = $("#tempOne");
+  var fiveDayHumid = $("#humidOne");
+  var fiveDayWind = $("#windOne");
+  var fiveDayTemp2 = $("#tempTwo");
+  var fiveDayHumid2 = $("#humidTwo");
+  var fiveDayWind2 = $("#windTwo");
+  var fiveDayTemp3 = $("#tempThree");
+  var fiveDayHumid3 = $("#humidThree");
+  var fiveDayWind3 = $("#windThree");
+  var fiveDayTemp4 = $("#tempFour");
+  var fiveDayHumid4 = $("#humidFour");
+  var fiveDayWind4 = $("#windFour");
+  var fiveDayTemp5 = $("#tempFive");
+  var fiveDayHumid5 = $("#humidFive");
+  var fiveDayWind5 = $("#windFive");
 
   loadcityName();
 
@@ -25,22 +40,64 @@ $(document).ready(function () {
         console.log(data.wind.speed);
         humidity.text("Humidity: " + data.main.humidity);
         console.log(data.main.humidity);
+        currentCity.text(data.name + currentDay);
+        console.log(data.name + currentDay);
       });
   });
+
+  var currentCity = $("#currentDay");
+
   searchButton.click(function () {
     console.log(inputCity.val());
-    var city = inputCity.val();
     var fiveDayForecastApi =
-      "https://api.openweathermap.org/data/2.5/forecast?q=" +
-      city +
-      "&appid=b0c8bb28d93f9d08fb9250393ad62966&units=imperial";
+      "https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&appid=b0c8bb28d93f9d08fb9250393ad62966&units=imperial";
     fetch(fiveDayForecastApi)
       .then((response) => response.json())
       .then(function (data) {
         console.log(data);
+        data = {
+          daily: [
+            fiveDayTemp.text("Temp: " + data.daily[0].temp),
+            console.log(data.daily[0].temp),
+            fiveDayHumid.text("Humidity: " + data.daily[0].humidity),
+            console.log(data.daily[0].humidity),
+            fiveDayWind.text("Wind: " + data.daily[0].wind_speed),
+            console.log(data.daily[0].wind_speed),
+
+            fiveDayTemp2.text("Temp: " + data.daily[1].temp),
+            console.log(data.daily[1].temp),
+            fiveDayHumid2.text("Humidity: " + data.daily[1].humidity),
+            console.log(data.daily[1].humidity),
+            fiveDayWind2.text("Wind: " + data.daily[1].wind_speed),
+            console.log(data.daily[1].wind_speed),
+
+            fiveDayTemp3.text("Temp: " + data.daily[2].temp),
+            console.log(data.daily[2].temp),
+            fiveDayHumid3.text("Humidity: " + data.daily[2].humidity),
+            console.log(data.daily[2].humidity),
+            fiveDayWind3.text("Wind: " + data.daily[2].wind_speed),
+            console.log(data.daily[2].wind_speed),
+
+            fiveDayTemp4.text("Temp: " + data.daily[3].temp),
+            console.log(data.daily[3].temp),
+            fiveDayHumid4.text("Humidity: " + data.daily[3].humidity),
+            console.log(data.daily[3].humidity),
+            fiveDayWind4.text("Wind: " + data.daily[3].wind_speed),
+            console.log(data.daily[3].wind_speed),
+
+            fiveDayTemp5.text("Temp: " + data.daily[4].temp),
+            console.log(data.daily[4].temp),
+            fiveDayHumid5.text("Humidity: " + data.daily[4].humidity),
+            console.log(data.daily[4].humidity),
+            fiveDayWind5.text("Wind: " + data.daily[4].wind_speed),
+            console.log(data.daily[4].wind_speed),
+          ],
+        };
       });
   });
 
+  
+  //
   function loadcityName() {
     searchedCities = JSON.parse(localStorage.getItem("lscityName"));
     if (searchedCities) {
@@ -83,22 +140,21 @@ $(document).ready(function () {
     forecastDeck(cityName);
   }
 
-  var currentDay = moment().format("MMMM Do, YYYY");
+  var currentDay = moment().format("l");
   $("#currentDay").text(currentDay);
 
   var dayOne = moment().add(1, "days").format("l");
-  $("#dayOne").text(dayOne.slice(0, 9));
+  $("#dayOne").text(dayOne.slice(0, 10));
 
   var dayTwo = moment().add(2, "days").format("l");
-  $("#dayTwo").text(dayTwo.slice(0, 9));
+  $("#dayTwo").text(dayTwo.slice(0, 10));
 
   var dayThree = moment().add(3, "days").format("l");
-  $("#dayThree").text(dayThree.slice(0, 9));
+  $("#dayThree").text(dayThree.slice(0, 10));
 
   var dayFour = moment().add(4, "days").format("l");
-  $("#dayFour").text(dayFour.slice(0, 9));
+  $("#dayFour").text(dayFour.slice(0, 10));
 
   var dayFive = moment().add(5, "days").format("l");
-  $("#dayFive").text(dayFive.slice(0, 9));
+  $("#dayFive").text(dayFive.slice(0, 10));
 });
-
